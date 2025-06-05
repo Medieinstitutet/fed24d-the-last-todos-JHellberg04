1461e28df32b99c55a36828302745d3df694d47a
Jonatan Hellberg <Jonatan.Hellberg@medieinstitutet.se>
2025-06-05 04:22:53 +0200

    feat: ✨Add todos

---
+++ src/data/initialTodos.ts
@@ -0,0 +1,9 @@
+import type { ITodo } from "../types/todo";
+
+
+export const initialTodos: ITodo[] = [
+  { id: 1, title: 'Städa rummet', isDone: false},
+  { id: 2, title: 'Skicka in React uppgiften', isDone: false},
+  { id: 3, title: 'Handla mat', isDone: false},
+  { id: 4, title: 'Gå på en promenad', isDone: false}
+]
