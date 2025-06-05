5c93f3332f2d0e3631be28b79f60a8a883f8411e
Jonatan Hellberg <Jonatan.Hellberg@medieinstitutet.se>
2025-06-05 05:06:24 +0200

    feat: ✨create "toggleDone" + Display TodoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import { TodoList } from "./components/TodoList";
+import { initialTodos } from "./data/initialTodos";
+import type { ITodo } from "./types/todo";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [todos, setTodos] = useState<ITodo[]>(initialTodos)
+
+  const toggleDone = (id: number) => {
+    setTodos(prevTodos =>
+      prevTodos.map(todo =>
+        todo.id === id ? {...todo, isDone: !todo.isDone } : todo
+      )
+    )
+  }
+  return <>
+  <TodoList todos={todos} onToggleDone={toggleDone} />
+  </>;
 }
 
 export default App;
+
