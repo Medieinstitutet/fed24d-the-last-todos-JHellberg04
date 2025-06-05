fee23b91539627aa3664e99b3c05df60dfd5fe9e
Jonatan Hellberg <Jonatan.Hellberg@medieinstitutet.se>
2025-06-05 05:31:31 +0200

    feat: ♿️Add alt text and aria-label to icons

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -10,9 +10,9 @@ export default function TodoItem({ todo, onToggleDone }: IProps) {
     <li>
       <span className="todo-title">{todo.title}</span>
       <button onClick={() => onToggleDone(todo.id)}>
-        <img src={!todo.isDone ? "/assets/icons/icon-complete.svg" : "/assets/icons/icon-undo.svg" } />
+        <img src={!todo.isDone ? "/assets/icons/icon-complete.svg" : "/assets/icons/icon-undo.svg" } alt={ !todo.isDone ? "markera som klar" : "avmarkera som klar"} aria-label={ !todo.isDone ? "markera sysslan som klar" : "avmarkera sysslan som klar"} />
       </button>
-      <button><img src="/assets/icons/icon-delete.svg" /></button>
+      <button><img src="/assets/icons/icon-delete.svg" alt="Radera" aria-label="Radera syssla" /></button>
     </li>
   )
 } 
